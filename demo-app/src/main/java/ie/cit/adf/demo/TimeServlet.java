0663be589337ab398c307702775295d99204e807
cathalcleary <cathalcleary@gmail.com>
2013-03-06 22:29:49 +0000

    Spring Web complete

--- demo-app/src/main/java/ie/cit/adf/demo/TimeServlet.java
+++ demo-app/src/main/java/ie/cit/adf/demo/TimeServlet.java
@@ -3,23 +3,33 @@ package ie.cit.adf.demo;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 @SuppressWarnings("serial")
 public class TimeServlet extends HttpServlet {
 
+	private TimeService timeSvc;
+
+	@Override
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		WebApplicationContext ctx = WebApplicationContextUtils.
+				getWebApplicationContext(servletContext);
+		timeSvc = (TimeService) ctx.getBean(TimeService.class);
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		PrintWriter writer = resp.getWriter();
-		ClassPathXmlApplicationContext ctx = 
-				new ClassPathXmlApplicationContext("classpath:context-config.xml");
-		TimeService timeSvc = (TimeService) ctx.getBean("TimeService");
 		java.util.Date currentDate = timeSvc.getCurrentDate();
 		writer.println("<HTML>");
 		writer.println("<body>");
@@ -28,6 +38,6 @@ public class TimeServlet extends HttpServlet {
 		writer.println("</body>");
 		writer.println("</HTML>");
 		writer.close();
-		ctx.close();
+
 		}
 }
